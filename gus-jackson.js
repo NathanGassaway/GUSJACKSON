141b6db7b90f3b0cd570b6f208aabac695da8b9f
NathanGassaway <Nathangway@gmail.com>
2020-12-24 19:14:06 -0500

    name switches

--- gus-jackson.js
+++ gus-jackson.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
 
-	slideProject = (function(){
-		$(".percent").click(function(){
-			$(this).fadeOut( 800, function(){
-				// $(this).toggle();
-				$(".dog-1").toggle(50)
-			});
+	landingClear = function(){
+		$(".landing-photo").click(function(){
+			$(this).hide();
 		});
-	});
+	};
+
+	slideProject = function(){
+		$(".percent-container").click(function(){
+			$(this).children().slideUp(100, function(){
+				// $(this).children().toggle(50)
+			});
+			$(this).children(".dog-1").toggle(100)
+			$(this).children(".dog-2").toggle(50)
+			$(this).children(".dog-3").toggle(50)
+			$(this).children(".dog-4").toggle(50)
+			$(this).children(".dog-5").toggle(50)
+			$(this).children(".dog-6").toggle(50)
+		});
+	};
+
+	landingClear();
 
 	slideProject();
+
 });
